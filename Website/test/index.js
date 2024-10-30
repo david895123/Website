60e63622a70fb49ff5b9cf28027d8923fa7ab268
david895123 <124035331+david895123@users.noreply.github.com>
2024-10-30 09:46:02 +0200

    Delete Website directory

--- Website/test/index.js
+++
@@ -1,8 +0,0 @@
-const {EventEmitter} = require('events');
-const eventEmitter = new EventEmitter();
-
-eventEmitter.on('cica', () =>{
-    console.log("cicagsags");
-})
-
-eventEmitter.emit('cica');
